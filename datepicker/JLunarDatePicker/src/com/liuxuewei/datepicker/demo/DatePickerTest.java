b40028bcfcb82d7a43eeb0bb384b2a46eba6ac0f
liu.xuewei <liu.xuewei@hotmail.com>
2013-03-05 09:20:05 +0000

    修正日期对齐

--- datepicker/JLunarDatePicker/src/com/liuxuewei/datepicker/demo/DatePickerTest.java
+++ datepicker/JLunarDatePicker/src/com/liuxuewei/datepicker/demo/DatePickerTest.java
@@ -15,7 +15,7 @@ public class DatePickerTest {
         JFrame frame = new JFrame();
         JLunarDatePicker lunarDatePicker = new JLunarDatePicker();
         Calendar wannaday = Calendar.getInstance(Locale.CHINA);
-		wannaday.set(1991, 0, 19);//注意Calendar的set方法中month 是从0开始，这里指定的是1991年9月19日
+		wannaday.set(1991, 0, 19);//注意Calendar的set方法中month 是从0开始，这里指定的是1991年1月19日
         Date date = wannaday.getTime();//获取指定日期
         lunarDatePicker.setDate(date);//设置要显示的农历日期
         frame.getContentPane().add(lunarDatePicker);
